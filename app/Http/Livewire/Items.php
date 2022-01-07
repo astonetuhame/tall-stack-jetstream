3190030aa0dd9a2cb7b9db7c35aa360fa2489241
Astone Tuhame <astonetuhame@gmail.com>
2022-01-07 15:57:50 +0300

    Add pagination

--- app/Http/Livewire/Items.php
+++ app/Http/Livewire/Items.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Item;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Items extends Component
 {
+    use WithPagination;
     public function render()
     {
-        return view('livewire.items');
+        $items = Item::where('user_id', auth()->user()->id)->paginate(10);
+        return view('livewire.items', ['items' => $items]);
     }
 }
